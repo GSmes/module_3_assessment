1b288d2d0746357e9da9688a0e04ac5637a35acd
Garrett <garrett.smestad@gmail.com>
2016-09-20 10:06:49 -0600

    Figured out why specs were not passing (typo)

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -13,11 +13,10 @@ RSpec.describe 'Items Endpoint', :type => :request do
     first_item = parsed_response.first
 
     expect(parsed_response.count).to eq(5)
-    expect(first_item[:name]).to eq(expected_item.name)
-    expect(first_item[:description]).to eq(expected_item.description)
-    expect(first_item[:image_url]).to eq(expected_item.image_url)
-    expect(first_item[:created_at]).to eq(expected_item.created_at)
-    expect(first_item[:updated_at]).to eq(expected_item.updated_at)
+    expect(first_item['id']).to eq(expected_item.id)
+    expect(first_item['name']).to eq(expected_item.name)
+    expect(first_item['description']).to eq(expected_item.description)
+    expect(first_item['image_url']).to eq(expected_item.image_url)
   end
 
   it 'should return a single item' do
@@ -30,11 +29,10 @@ RSpec.describe 'Items Endpoint', :type => :request do
 
     parsed_response = JSON.parse(response.body)
 
-    expect(parsed_response[:name]).to eq(expected_item.name)
-    expect(parsed_response[:description]).to eq(expected_item.description)
-    expect(parsed_response[:image_url]).to eq(expected_item.image_url)
-    expect(parsed_response[:created_at]).to eq(expected_item.created_at)
-    expect(parsed_response[:updated_at]).to eq(expected_item.updated_at)
+    expect(parsed_response['id']).to eq(expected_item.id)
+    expect(parsed_response['name']).to eq(expected_item.name)
+    expect(parsed_response['description']).to eq(expected_item.description)
+    expect(parsed_response['image_url']).to eq(expected_item.image_url)
   end
 
   it 'should delete an item' do
@@ -65,8 +63,8 @@ RSpec.describe 'Items Endpoint', :type => :request do
 
     new_item = JSON.parse(response.body)
 
-    expect(new_item[:name]).to eq('Gameboy')
-    expect(new_item[:description]).to eq('It is a Gameboy')
-    expect(new_item[:image_url]).to eq('http://www.url.com')
+    expect(new_item['name']).to eq('Gameboy')
+    expect(new_item['description']).to eq('It is a Gameboy')
+    expect(new_item['image_url']).to eq('http://www.url.com')
   end
 end
